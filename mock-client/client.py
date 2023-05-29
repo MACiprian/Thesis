ddb51a21f9e31e64cd43d10d8d797bfa427110a4
Alex Apostolescu <alexx.apostolescu@gmail.com>
2023-05-29 20:02:19 +0300

    Use docker compose for mock-client and server

--- mock-client/client.py
+++ mock-client/client.py
@@ -1,13 +1,19 @@
+import os
 import time
-import requests as r
+import requests
 import random
 
+
 if __name__ == '__main__':
+    SERVER_URL=os.getenv('SERVER_URL', '')
+    if not SERVER_URL:
+        raise Exception('SERVER_URL not set')
+
     while True:
         post_data = {
             "temp": 22 + random.random(),
             "humidity": 51 + random.random() * 3,
             "heatIndex": 24 + random.random() * 2
         }
-        r.post('http://localhost:8000/data', json=post_data)
+        requests.post(SERVER_URL + "/data", json=post_data)
         time.sleep(2)
--- server/server.py
+++ server/server.py
@@ -1,14 +1,19 @@
+import os
+import sys
 from flask import Flask, request
-import json
 
 app = Flask(__name__)
 
 @app.route('/data', methods=['POST'])
 def handle_post_request():
     json_data = request.get_json()
-    print(json_data)
+    print(json_data, file=sys.stderr)
 
     return 'POST request received'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    SERVER_PORT=os.getenv('SERVER_PORT', '')
+    if not SERVER_PORT:
+        raise Exception('SERVER_PORT not set')
+    
+    app.run(host='0.0.0.0', port=SERVER_PORT, debug=False)
