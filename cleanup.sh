777a087cde28e39d6d7a14a1880d1634f0c970bd
Ciceps <mitroi.ciprian.cuza.89@gmail.com>
2023-06-01 01:34:08 +0300

    Add grafana container
    The dashboard is automatically loaded.

--- cleanup.sh
+++ cleanup.sh
@@ -2,5 +2,4 @@
 
 # stop the cluster and remove stored data
 docker compose down
-docker volume ls | grep influxdb-data | awk '{print $2}' | xargs docker volume rm
-
+rm -rf grafana/grafana-storage influxdb/influxdb-storage
